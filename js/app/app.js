227cf5c54fa4279459c1bd60812a2c56493e6b74
88mpg <fratino.a@gmail.com>
2016-03-29 12:34:40 -0400

    add save functionality

--- js/app/app.js
+++ js/app/app.js
@@ -1,9 +1,15 @@
-$('button').on('click', function() {
+$('#convert').on('click', function() {
   var tableData = $('#data').val();
   $('#rawTable').empty().append(tableData);
   var table = $('table').tableToJSON();
   $('#output').text(JSON.stringify(table, null, 2));
   // $('table').hide();
+
+  var json = $("#output").val();
+  var blob = new Blob([json], {type:'application/json'});
+  var downloadLink = document.getElementById("save");
+  downloadLink.download = "data";
+  downloadLink.href = URL.createObjectURL(blob);
 });
 
 var client = new ZeroClipboard($('#copy'));
